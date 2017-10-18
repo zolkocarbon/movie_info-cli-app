64413ef6897e181818e02fce827e6650e1031796
zolkocarbon <eathena@hotmail.com>
2017-10-18 16:20:54 +0000

    All lib files executing

--- lib/movie_info.rb
+++ lib/movie_info.rb
@@ -1,5 +1,12 @@
-require "movie_info/version"
+require 'rest-client'
+require 'json'
+require 'pry'
 
+
+require_relative "./movie_info/version"
 module MovieInfo
-  # Your code goes here...
+
 end
+#this order matters, has to be afer module
+require_relative "./movie_info/api"
+require_relative "./movie_info/cli"
