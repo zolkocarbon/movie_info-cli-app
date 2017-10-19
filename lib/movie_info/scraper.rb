b48c36a0a8df295b1f1e8e9088c4b27dabb11340
zolkocarbon <eathena@hotmail.com>
2017-10-19 15:04:56 +0000

    Created class method to scrape plot page of movie instance.

--- lib/movie_info/scraper.rb
+++ lib/movie_info/scraper.rb
@@ -8,20 +8,23 @@ class MovieInfo::Scraper
 #Select elements containing data needed to crate each movie and place
 #each match into array element
   def self.movies_array
-    test = self.scrape_page.css("div.MoviePostersGrid-poster")
-
+    self.scrape_page.css("div.MoviePostersGrid-poster")
   end
 
 #for each array element make an instance of the movie_data
 #passing in the title and url to movie instance initialize as arguments
   def self.make_movie
     movies_array.each do |m|
-      binding.pry
       MovieInfo::Movie.new(
       m.css("h3").text,
-      "https://www.amctheatres.com/movies#{m.css("a").attr("href").text}"
+      "https://www.amctheatres.com#{m.css("a").attr("href").text}"
       )
     end
+  end
+
+  def self.scrape_plot(link)
+    plot = Nokogiri::HTML(open(link)).css("div.Intro-text p").text
+    #binding.pry
   end
 
 end
