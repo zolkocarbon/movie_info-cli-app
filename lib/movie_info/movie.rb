65af73c4a9be389ce973943c4bfa4afa6c588f16
zolkocarbon <eathena@hotmail.com>
2017-10-19 15:48:08 +0000

    Plot and find_movie methods finished.

--- lib/movie_info/movie.rb
+++ lib/movie_info/movie.rb
@@ -1,13 +1,24 @@
-
 class MovieInfo::Movie
 
   attr_accessor :title, :link, :plot, :duration
+  @@all = [] #holds all the movie instances
 
   def initialize(title, link)
     @title = title
     @link = link
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_movie(input)
+    self.all[input-1]
+  end
+
+  def plot
+    @plot ||= MovieInfo::Scraper.scrape_plot(self.link)
   end
 
 end
-#Main page to scrape: https://www.amctheatres.com/movies
-#Selector to scrape movies:
