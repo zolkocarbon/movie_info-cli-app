96c2b4abf5e39ce04352484d91cdc02c090a6442
zolkocarbon <eathena@hotmail.com>
2017-10-19 14:19:30 +0000

    Scraper tested and working.

--- lib/movie_info/scraper.rb
+++ lib/movie_info/scraper.rb
@@ -1,23 +1,25 @@
 class MovieInfo::Scraper
 
 #Get HTML from page
-  def scrape_page
+  def self.scrape_page
     Nokogiri::HTML(open("https://www.amctheatres.com/movies"))
   end
 
 #Select elements containing data needed to crate each movie and place
 #each match into array element
-  def movie_array
-    scrape_page.css("div.PosterSlide")
+  def self.movies_array
+    test = self.scrape_page.css("div.MoviePostersGrid-poster")
+
   end
 
 #for each array element make an instance of the movie_data
 #passing in the title and url to movie instance initialize as arguments
-  def make_movie
+  def self.make_movie
     movies_array.each do |m|
+      binding.pry
       MovieInfo::Movie.new(
-      m.css("div.MoviePostersGrid-text h3").text,
-      "https://www.amctheatres.com/movies#{m.css("div.PosterSlide a.Link").attribute("href").text}"
+      m.css("h3").text,
+      "https://www.amctheatres.com/movies#{m.css("a").attr("href").text}"
       )
     end
   end
