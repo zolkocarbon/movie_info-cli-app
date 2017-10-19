94089cb4c7e0efd121ecc02d1b1b2b6bd1c03804
zolkocarbon <eathena@hotmail.com>
2017-10-19 15:19:06 +0000

    Finalized start method to work with plot option

--- lib/movie_info/cli.rb
+++ lib/movie_info/cli.rb
@@ -1,19 +1,19 @@
 class MovieInfo::CLI
 
   def call
-    MovieInfo::Movie.new
+    MovieInfo::Scraper.make_movie
     start
   end
 
   def list
-    puts "AMC Theaters Feature Movies"
+    puts "These are the feature movies at our theater."
     MovieInfo::Movie.all.each.with_index(1) do |movie_instance, i|
       puts "#{i}. #{movie_instance.title}"
     end
   end
 
-
   def start
+    puts "Welcome to AMC Theaters"
     list
     input = nil
     while input != "exit"
@@ -21,12 +21,12 @@ class MovieInfo::CLI
       puts "To see the list again, enter 'list'."
       puts "To exit the program, enter 'exit'."
       input = gets.strip.downcase
-      if input.between?(1..MovieInfo::Movie.all-1)
-        movie_plot(input) #will call scrape_plot
+      if input.to_i.between?(1, MovieInfo::Movie.all.length-1)
+        puts "Movie Plot - #{MovieInfo::Movie.find_movie(input.to_i).title}"
+        puts "#{MovieInfo::Movie.find_movie(input.to_i).plot}" #will call find_movie and scrape_plot
+        puts "-------------------------------------------------"
       elsif input == "list"
         list
-      elsif input != "exit"
-        puts "The input you entered is not valid."
       end
     end
     puts "Have a nice day."
