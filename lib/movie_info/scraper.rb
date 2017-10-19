be473edbd913a83f352a38445011d6e81f563c39
zolkocarbon <eathena@hotmail.com>
2017-10-19 13:41:57 +0000

    Added comments to page.

--- lib/movie_info/scraper.rb
+++ lib/movie_info/scraper.rb
@@ -1,13 +1,18 @@
 class MovieInfo::Scraper
 
+#Get HTML from page
   def scrape_page
     Nokogiri::HTML(open("https://www.amctheatres.com/movies"))
   end
 
+#Select elements containing data needed to crate each movie and place
+#each match into array element
   def movie_array
     scrape_page.css("div.PosterSlide")
   end
 
+#for each array element make an instance of the movie_data
+#passing in the title and url to movie instance initialize as arguments
   def make_movie
     movies_array.each do |m|
       MovieInfo::Movie.new(
